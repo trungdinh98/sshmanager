e1dbe9bce8f6ccafb3c9e79b54071850cbbe7d06
trungdinh98 <batrungdinh33@gmail.com>
2019-10-25 00:38:35 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+
 class App extends Component{
    render(){
       return(
-         <div>
-            <h1>Hello World</h1>
-         </div>
+        <div>
+           <h1>Welcome to SSH Manager</h1>
+        </div>
       );
    }
 }
