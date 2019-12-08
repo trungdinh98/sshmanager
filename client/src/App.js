717f53d6556b7514a2ec2e0a3fd9299c94e49575
trungdinh98 <batrungdinh33@gmail.com>
2019-12-08 17:29:14 +0700

    Update Keys Screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Header from './Header/Header.js';
+import Keys from './Keys/Keys.js';
 
 class App extends React.Component {
   render () {
     return (
-      <Header />
+        <div style={{float:'none'}}>
+            <Header />
+            <Keys />
+        </div>
     );
   }
 }
--- client/src/Keys/Keys.js
+++ client/src/Keys/Keys.js
@@ -4,7 +4,57 @@ import './Keys.css';
 class Keys extends React.Component {
   render () {
     return (
-      <div>This is Keys page</div>
+      <div>
+          <div className="top-content">
+              <input className="key-search" type="text" placeholder="Find by key ID or key name" />
+              <button className="new-key">New Key</button>
+              <button className="delete-key">Delete Key</button>
+          </div>
+          <div className="bot-content">
+            <table>
+              <thead>
+                <tr>
+                    <th className="check-box">
+                        <input type="checkbox" />
+                    </th>
+                    <th>Key Name</th>
+                    <th>Key ID</th>
+                    <th>Key Value</th>
+                    <th>Registered Time</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                    <th className="check-box">
+                        <input type="checkbox" />
+                    </th>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                </tr>
+                <tr>
+                    <th className="check-box">
+                        <input type="checkbox" />
+                    </th>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                </tr>
+                <tr>
+                    <th className="check-box">
+                        <input type="checkbox" />
+                    </th>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                    <td>Table cell</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+      </div>
     );
   }
 }
