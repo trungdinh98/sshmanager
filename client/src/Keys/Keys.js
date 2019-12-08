88b45605c24e88aa9ae8dbeb0e56341008c2c144
trungdinh98 <batrungdinh33@gmail.com>
2019-12-08 12:34:38 +0700

    Update Header

---
+++ client/src/Keys/Keys.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Keys.css';
+
+class Keys extends React.Component {
+  render () {
+    return (
+      <div>This is Keys page</div>
+    );
+  }
+}
+
+export default Keys;
